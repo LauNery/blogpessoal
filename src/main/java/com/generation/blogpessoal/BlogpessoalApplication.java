8a09f8c79bf1b8334bf240308e3293d026d5fcdd
Laura Nery <laura.olivernery@gmail.com>
2025-04-30 17:48:50 -0300

    novas atualizações

--- src/main/java/com/generation/blogpessoal/BlogpessoalApplication.java
+++ src/main/java/com/generation/blogpessoal/BlogpessoalApplication.java
@@ -2,18 +2,12 @@ package com.generation.blogpessoal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.util.Collections;
 
 @SpringBootApplication
 public class BlogpessoalApplication {
 
-    public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(BlogpessoalApplication.class);
-        String port = System.getenv("PORT");
-        if (port == null) {
-            port = "8080";
-        }
-        app.setDefaultProperties(Collections.singletonMap("server.port", port));
-        app.run(args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(BlogpessoalApplication.class, args);
+	}
+
 }
